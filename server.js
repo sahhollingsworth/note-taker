3413c5bd7a4b6d92e980234408b0b0af9c16444c
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-10-21 16:14:20 -0700

    Fix: Posting a note added deleted notes.
    When adding a new note, the post request wasn't referencing source of truth, db.json. as a result, when a user posted a new note, any note deleted since last post would also return.

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const { v4: uuidv4 } = require('uuid');
 // Import path module that provides utilities for working with file and directory paths
 const path = require("path");
 // Import json (acting database) object containing all notes
-var noteDB = require("./db/db.json");
-
-console.log(noteDB);
-console.log(typeof noteDB);
+const noteDB = require("./db/db.json");
 
 const app = express();
 
@@ -42,8 +39,15 @@ app.get('/api/notes', (req, res) => {
             console.error(err)
         }
         else {
+            // Parse notes data into array
             let parsedNotes = JSON.parse(noteDB);
+
+            // send updated notes data as json object to client
             res.json(parsedNotes)
+
+            // Testing - notes content at each stage
+            console.log("Output of getting notes, exact match to source of truth");
+            console.log(parsedNotes);
         }
     })
 }); 
@@ -54,28 +58,44 @@ app.post('/api/notes', (req, res) => {
     req.body.id = uuidv4();
     // Set the request body (title, text, id) as the new note to be added
     const newNote = req.body;
-    // See the newnote contents
+    
+    // Testing - confirmation of note
+    console.log("See the new note")
     console.log(newNote);
-    
-    //Add new note to notes database
-    noteDB.push(newNote);
-    
-    // Write updated Notes to the notes db.json file
-    fs.writeFile("./db/db.json", JSON.stringify(noteDB), (writeError) =>
-        writeError
-            // Write an error to the web console (and `stderr`)
-            ? console.error(writeError)
-            // Write an success message to the web console
-            : console.info("Added new note successfully!")
-    )
-    // send updated json object to client
-    res.json(noteDB);
+
+    // Pull in notes data from the source of truth
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) {
+            console.error(err)
+        }
+        else {
+            // Parse notes data into array
+            let parsedNotes = JSON.parse(data);
+            // Add the new note as the last object in the array
+            parsedNotes.push(newNote);
+
+            // Testing - notes content at each stage
+            console.log("Output of posting a note, all notes including new");
+            console.log(parsedNotes);
+
+            // Write the updated notes data back to the source of truth
+            fs.writeFile("./db/db.json", JSON.stringify(parsedNotes), (writeError) =>
+            writeError
+                // Write an error to the web console (and `stderr`)
+                ? console.error(writeError)
+                // Write an success message to the web console
+                : console.info("Added new note successfully!")
+            )
+            // send updated notes data as json object to client
+            res.json(parsedNotes);
+        }
+    })
 });
 
 // Delete request to delete a note
 app.delete('/api/notes/:id', (req, res) => {
-    // for testing - delete request running
-    console.log('Got a DELETE request at /notes');
+    // Testing - confirming request received
+    console.log('Received Delete request at /notes');
 
     // retreive the id from the request body 
     const id = req.params.id;
@@ -83,7 +103,8 @@ app.delete('/api/notes/:id', (req, res) => {
     // Review all note id's in db.json and only include those that don't have the same id as the one being deleted
     updatedNotes = noteDB.filter(note => note.id != id);
 
-    // for testing - confirm removal of note
+    // Testing - notes content at each stage
+    console.log("Output of deleting a note, all notes excluded note deleted");
     console.log(updatedNotes);
 
     // update notes db file with the updated list of notes
